9359f956e886258ea478d199fdecfc234d2db64e
siravit <siravit.t@arise.tech>
2024-01-11 23:26:24 +0700

    edit nvm script

--- scripts/setup-dev-dependencies.sh
+++ scripts/setup-dev-dependencies.sh
@@ -3,13 +3,13 @@ cd "$(dirname "$0")"
 
 set -euo pipefail
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 # Install node_modules
 printf "\360\237\215\272\t Installing development dependencies...  \n"
-nvm install 20.10.0
-nvm use v20.10.0
+# nvm install 20.10.0
+# nvm use v20.10.0
 
 cd ../user-fe
 pnpm install
