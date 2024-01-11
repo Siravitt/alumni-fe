cfb994fdb4f6f379d0b788e72ec13084af90ca54
siravit <siravit.t@arise.tech>
2024-01-11 23:06:49 +0700

    edit nvm script

--- scripts/setup-dev-dependencies.sh
+++ scripts/setup-dev-dependencies.sh
@@ -3,6 +3,9 @@ cd "$(dirname "$0")"
 
 set -euo pipefail
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
 # Install node_modules
 printf "\360\237\215\272\t Installing development dependencies...  \n"
 nvm install 20.10.0
