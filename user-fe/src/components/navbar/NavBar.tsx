b564254618ff02c2ce139b7f1d35c615e7faff37
Thanatip-iceJ <p.thanatip1@gmail.com>
2024-01-22 13:46:12 +0700

    added darkmode

--- user-fe/src/components/navbar/NavBar.tsx
+++ user-fe/src/components/navbar/NavBar.tsx
@@ -9,22 +9,21 @@ import ThemeButton from "./ThemeButton"
 
 function NavBar() {
   return (
-    <div className="flex items-center justify-between py-3 sm:px-[1rem] md:px-[2rem] lg:px-[3rem] xl:px-[3rem] 2xl:px-[15rem]">
+    <div className="flex items-center justify-between py-3 sm:px-[1rem] md:px-[2rem] lg:px-[3rem] xl:px-[3rem] 2xl:px-[10rem]">
       <Logo />
       <div className="flex gap-[2rem]">
-        <div className="hidden gap-[2rem] sm:hidden md:hidden lg:flex xl:flex 2xl:flex">
+        <div className="hidden gap-[2rem] sm:hidden md:hidden lg:hidden xl:flex 2xl:flex">
           <NavBarMenu title="About" />
           <NavBarMenu />
           <NavBarMenu />
           <NavBarMenu title="Contact Us" />
         </div>
         <div className="flex gap-[1rem]">
-          <div className="hidden gap-3 sm:hidden md:flex lg:flex xl:flex 2xl:flex">
+          <div className="hidden gap-3 sm:hidden md:hidden lg:flex xl:flex 2xl:flex">
             <LoginButton />
             <ThemeButton />
           </div>
-          <div className="sm:block md:block lg:hidden xl:hidden 2xl:hidden">
-            {/* <MenuIcon fontSize="large" /> */}
+          <div className="sm:block md:block xl:hidden 2xl:hidden">
             <MobileSideBar />
           </div>
         </div>
